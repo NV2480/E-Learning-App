dad58599fadf12030e063a809c13afd1fb9b7dc7
NV2480 <61974610+NV2480@users.noreply.github.com>
2021-01-12 01:24:38 +0530

    Update settings.py

--- educa/settings.py
+++ educa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 's$7^ecg8&954xl3bk=60jf6crvkq7fwq-)eba)wmqoovx!uyk1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','nvedu123.herokuapp.com']
 
 
 # Application definition
